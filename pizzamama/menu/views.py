d29125197e8966c758918e9b2250012fa9ed5d31
ABOURA Mohammed Ilyes <abouramohammedilyes@gmail.com>
2023-07-01 15:52:48 +0100

    Some minor modifications

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pizza
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Les pizzas")
+    pizzas = Pizza.objects.all()
+    pizzas_noms = [pizza.nom for pizza in pizzas]
+    pizzas_noms_str = ", ".join(pizzas_noms)
+    return HttpResponse("Les pizzas : " + pizzas_noms_str)
